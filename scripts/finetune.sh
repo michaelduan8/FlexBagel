3c336c96f81bdab7a02692079fc47430ca4b6717
Mic Dun <micdun@Mics-MacBook-Pro.local>
2026-03-09 04:25:45 -0700

    reenable deepspeed

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -13,8 +13,7 @@ echo Training med_r1 expert
     #     continue
     # fi
 # TODO: setting master port for now since I'm running another deepspeed job
-# PYTHONPATH=. deepspeed --master_port=29501 --num_gpus=$num_gpus train/finetune.py \
-PYTHONPATH=. python train/finetune.py \
+PYTHONPATH=. deepspeed --master_port=29501 --num_gpus=$num_gpus train/finetune.py \
     --run_id "med_r1_qwen2_5-1_5b_expert" \
     --model $model_store/med_r1_qwen25_moe_2x1b_instruct \
     --datasets "$datastore/med_r1/med_r1_traces.jsonl" \
@@ -32,27 +31,27 @@ PYTHONPATH=. python train/finetune.py \
     --save_n_epochs 1 \
     --dataset_num_proc 6 \
     --skip_eval \
-    --train_expert_idx 1
-    #--deepspeed "$ds_config" \
+    --train_expert_idx 1 \
+    --deepspeed "$ds_config"
 
-# echo Training fino1 expert
-# PYTHONPATH=. deepspeed --master_port=29501 --num_gpus=$num_gpus train/finetune.py \
-#     --run_id "fino1_qwen2_5-1_5b_expert" \
-#     --model $model_store/fino1_qwen25_moe_2x1b_instruct \
-#     --datasets "$datastore/fino1_finqa/fino1_finqa_traces.jsonl" \
-#     --num_train_epochs 5 \
-#     --per_device_train_batch_size 1 \
-#     --per_device_eval_batch_size 1 \
-#     --gradient_accumulation_steps 16 \
-#     --logging_steps 10 \
-#     --learning_rate 2e-5 \
-#     --warmup_ratio 0.1 \
-#     --gradient_checkpointing \
-#     --max_length 20000 \
-#     --run_seed $run_seed \
-#     --run_output_dir "${OUTPUT_DIR}/text_only_warmup/fino1_qwen2_5-1_5b/" \
-#     --save_n_epochs 1 \
-#     --dataset_num_proc 6 \
-#     --deepspeed "$ds_config" \
-#     --skip_eval \
-#     --train_expert_idx 1
+echo Training fino1 expert
+PYTHONPATH=. deepspeed --master_port=29501 --num_gpus=$num_gpus train/finetune.py \
+    --run_id "fino1_qwen2_5-1_5b_expert" \
+    --model $model_store/fino1_qwen25_moe_2x1b_instruct \
+    --datasets "$datastore/fino1_finqa/fino1_finqa_traces.jsonl" \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --logging_steps 10 \
+    --learning_rate 2e-5 \
+    --warmup_ratio 0.1 \
+    --gradient_checkpointing \
+    --max_length 20000 \
+    --run_seed $run_seed \
+    --run_output_dir "${OUTPUT_DIR}/text_only_warmup/fino1_qwen2_5-1_5b/" \
+    --save_n_epochs 1 \
+    --dataset_num_proc 6 \
+    --deepspeed "$ds_config" \
+    --skip_eval \
+    --train_expert_idx 1
